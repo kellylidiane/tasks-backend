2ab0e29517f0e2c78fa0839f92ff54720cc62f5e
Kelly Lidiane <k3gonzaga@gmail.com>
2023-12-01 13:56:33 -0300

    first commit

---
+++ migrations/20231129155704_create_table_tasks.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("tasks", (table) => {
+    table.increments("id").primary();
+    table.string("description").notNull();
+    table.datetime("estimatedDate");
+    table.datetime("doneAt");
+    table.integer("userId").references("id").inTable("users").notNull();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("tasks");
+};
